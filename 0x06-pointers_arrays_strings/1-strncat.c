c16d02f06a641e6dfe109793cce38a651d9085f7
Johnkehike <kehikejohnbull@yahoo.com>
2022-09-22 00:33:19 -0700

    great doings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *
+ * comments
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return(strncat(dest, src, n));
+	return(strncat(dest, src, n));
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *
+ * Strncpy command
+ *
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return(strncpy(dest, src, n));
+	return(strncpy(dest, src, n));
+}
