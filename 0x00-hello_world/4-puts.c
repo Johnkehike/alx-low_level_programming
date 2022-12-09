6a7008d8a9a7068562cf4183191d1e40f1907843
Johnkehike <kehikejohnbull@yahoo.com>
2022-12-08 19:22:06 -0800

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
-#include	<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Returns 0
  */
-int main(void){
-	char str[51];
-
-	strcpy(str, "Programming is like building a multilingual puzzle");
-
-	puts(str);
-
-	return(0);
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
