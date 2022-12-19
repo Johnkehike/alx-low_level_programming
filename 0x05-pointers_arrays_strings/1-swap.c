41bc84e6cc8e19e0b89276e8f0d5e6b77466488b
Johnkehike <kehikejohnbull@yahoo.com>
2022-12-19 12:22:20 -0800

    hi

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,17 @@
 #include "main.h"
-
-
+/**
+ * swap_int - entry
+ * @a:this
+ * @b: that
+ *
+ * description: done
+ *
+ * Return: 0
+ */
 void swap_int(int *a, int *b)
 {
 	int var;
 	var=*a;
 	*a=*b;
 	*b=var;
-
 }
