710610db4df9035c2eddc04d6c12f58075aef0b7
Johnkehike <kehikejohnbull@yahoo.com>
2022-12-19 18:51:38 -0800

    hi

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,10 +17,10 @@ void print_rev(char *s)
 	char temp;
 	int i, lenght, lastItem, firstItem;
 
-	lenght=strlen(s);
-	firstItem=0;
-	lastItem=lenght-1;
-	for(i=firstItem; i<=lastItem; i++)
+	lenght = strlen(s);
+	firstItem = 0;
+	lastItem = lenght - 1;
+	for (i = firstItem; i <= lastItem; i++)
 	{
 		temp=s[i];
 		s[i]=s[lastItem];
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,25 @@
 #include "main.h"
 #include <string.h>
-
+/**
+ * rev_string - entry
+ * @s:done
+ * description: does
+ *
+ * Return:
+ */
 void rev_string(char *s)
 {
 	char temp;
 	int i, lenght, lastItem, firstItem;
 
-        lenght=strlen(s);
-        firstItem=0;
-        lastItem=lenght-1;
-        for(i=firstItem; i<=lastItem; i++)
-        {
-                temp=s[i];
-                s[i]=s[lastItem];
-                s[lastItem]=temp;
-                lastItem--;
-        }
-}	
+	lenght=strlen(s);
+	firstItem=0;
+	lastItem=lenght-1;
+	for(i=firstItem; i<=lastItem; i++)
+	{
+		temp=s[i];
+		s[i]=s[lastItem];
+		s[lastItem]=temp;
+		lastItem--;
+	}
+}
