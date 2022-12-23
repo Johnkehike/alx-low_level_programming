f38ccda206a57eb3cfd06b9e24319dd1b057ff2a
Johnkehike <kehikejohnbull@yahoo.com>
2022-12-22 19:49:50 -0800

    hi

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,16 @@
 #include "main.h"
 #include <string.h>
 /**
+ * *_strcat - entry
+ * @dest:this
+ * @src:that
+ * description: those
  *
  * Concatenate Strings
+ * Return:0
  *
  */
 char *_strcat(char *dest, char *src)
 {
-	return(strcat(dest,src));
+	return (strcat(dest,src));
 }
