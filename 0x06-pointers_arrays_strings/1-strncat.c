54a7954ab78044eead2f93c6f5a31210ec205b53
Johnkehike <kehikejohnbull@yahoo.com>
2022-12-22 19:54:33 -0800

    hi

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,14 @@
 #include "main.h"
 #include <string.h>
-
 /**
+ * *_strncat - entry
+ * @dest: this
+ * @src: that
+ * @n: those
+ * description: them
  *
  * comments
+ * Return: watch
  *
  */
 char *_strncat(char *dest, char *src, int n)
